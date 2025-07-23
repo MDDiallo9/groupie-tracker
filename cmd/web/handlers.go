b0475a705522820dba1fc22ff12145fbf696b576
Mohamed LN-WSL <114567817+MDDiallo9@users.noreply.github.com>
2025-07-23 16:12:38 +0200

    Added GetArtist handler to fetch all data

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,53 +1,51 @@
 package main
 
 import (
-	"net/http"
-	"html/template"
-	"log"
 	"encoding/json"
+	"html/template"
 	"io"
-	/* "strconv" */
-)
+	"log"
+	"net/http"
+	/* "strconv" */)
 
 type Artist struct {
-    ID           int      `json:"id"`
-    Image        string   `json:"image"`
-    Name         string   `json:"name"`
-    Members      []string `json:"members"`
-    CreationDate int      `json:"creationDate"`
-    FirstAlbum   string   `json:"firstAlbum"`
-    Locations    string   `json:"locations"`
-    ConcertDates string   `json:"concertDates"`
-    Relations    string   `json:"relations"`
+	ID               int      `json:"id"`
+	Image            string   `json:"image"`
+	Name             string   `json:"name"`
+	Members          []string `json:"members"`
+	CreationDate     int      `json:"creationDate"`
+	FirstAlbum       string   `json:"firstAlbum"`
+	LocationsLink    string   `json:"locations"`
+	Locations        []string
+	ConcertDatesLink string `json:"concertDates"`
+	ConcertDates     []string
+	RelationsLink    string `json:"relations"`
+	Relations        map[string][]string
 }
 
-func (app *application) home(w http.ResponseWriter,r *http.Request){
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	link := "https://groupietrackers.herokuapp.com/api/artists"
-    response, err := http.Get(link)
-
-    if err != nil {
-        log.Print(err.Error())
-    }
+	response, err := http.Get(link)
+	if err != nil {
+		log.Print(err.Error())
+	}
 
 	var artists []Artist
 
-    
+	if err != nil {
+		log.Print(err.Error())
+	}
+	defer response.Body.Close()
 
-    if err != nil {
-        log.Print(err.Error())
-    }
-    defer response.Body.Close()
+	responseData, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    responseData, err := io.ReadAll(response.Body)
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    err = json.Unmarshal(responseData, &artists)
-    if err != nil {
-        log.Fatal(err)
-    }
-    
+	err = json.Unmarshal(responseData, &artists)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	ts, err := template.ParseFiles("./templates/home.html")
 	if err != nil {
@@ -55,7 +53,7 @@ func (app *application) home(w http.ResponseWriter,r *http.Request){
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
-	
+
 	err = ts.Execute(w, artists)
 	if err != nil {
 		log.Print(err.Error())
@@ -63,37 +61,10 @@ func (app *application) home(w http.ResponseWriter,r *http.Request){
 	}
 }
 
-func (app *application) Artist(w http.ResponseWriter,r *http.Request){
+func (app *application) Artist(w http.ResponseWriter, r *http.Request) {
 	id := r.URL.Query().Get("id")
-	
-	link := "https://groupietrackers.herokuapp.com/api/artists/" + id
 
-	response, err := http.Get(link)
-
-    if err != nil {
-        log.Print(err.Error())
-    }
-
-	var artist Artist
-
-    
-
-    if err != nil {
-        log.Print(err.Error())
-    }
-    defer response.Body.Close()
-
-    responseData, err := io.ReadAll(response.Body)
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    err = json.Unmarshal(responseData, &artist)
-    if err != nil {
-        log.Fatal(err)
-    }
-
-
+	artist := app.GetArtist(id)
 
 	ts, err := template.ParseFiles("./templates/artist.html")
 	if err != nil {
@@ -101,7 +72,7 @@ func (app *application) Artist(w http.ResponseWriter,r *http.Request){
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
-	
+
 	err = ts.Execute(w, artist)
 	if err != nil {
 		log.Print(err.Error())
