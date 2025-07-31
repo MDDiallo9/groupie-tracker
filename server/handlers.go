1048a68d364576af463781d186249c9493418a82
Mohamed LN-WSL <114567817+MDDiallo9@users.noreply.github.com>
2025-07-31 14:10:43 +0200

    Ajout fonctions pour FilterBy

--- server/handlers.go
+++ server/handlers.go
@@ -10,10 +10,41 @@ import (
 	/* "strconv" */)
 
 func home(w http.ResponseWriter, r *http.Request) {
-	testFilter := api.Filter{ /* CreationDate: []int{1970,2000}, */ /* Members: map[int]bool{3:true,4:true,5:true,6:true,7:true}, */ Location: "USA"}
+	var filter api.Filter
+	if r.Method == "POST" {
+		r.ParseForm()
+		/* log.Printf("%+v\n", r.Form) */
+		fad, _ := strconv.Atoi(r.Form["FirstAlbumDate"][0])
+		cdMin, _ := strconv.Atoi(r.Form["creationDate"][0])
+		cdMax := 2025
+		members := map[int]bool{
+			1: false,
+			2: false,
+			3: false,
+			4: false,
+			5: false,
+			6: false,
+			7: false,
+		}
+		for _, num := range r.Form["members"] {
+			n, _ := strconv.Atoi(num)
+			members[n] = true
+		}
+		filter = api.Filter{
+			Location:       r.Form["Location"][0],
+			FirstAlbumDate: fad,
+			Members:        members,
+			CreationDate:   []int{cdMin, cdMax},
+		} // Besoin de recharger home avec le api.FilterBy(artists,filter)
+	}
+	
 	artists := api.GetArtists()
 
-	test := api.FilterBy(artists, testFilter)
+	if isFilterFilled(filter) {
+		artists = api.FilterBy(artists, filter)
+		log.Println(artists,filter)
+	}
+
 
 	ts, err := template.ParseFiles("./templates/home.html", "./templates/partials/base.html", "./templates/partials/footer.html", "./templates/partials/head.html")
 	if err != nil {
@@ -22,7 +53,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = ts.ExecuteTemplate(w, "base.html", test)
+	err = ts.ExecuteTemplate(w, "base.html", artists)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
@@ -52,4 +83,19 @@ func Artist(w http.ResponseWriter, r *http.Request) {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
+}
+
+func isFilterFilled(f api.Filter) bool {
+    for _, v := range f.Members {
+        if v {
+            return true
+        }
+    }
+    if f.Location != "" || f.FirstAlbumDate != 0 {
+        return true
+    }
+    if len(f.CreationDate) == 2 && (f.CreationDate[0] != 0 || f.CreationDate[1] != 2025) {
+        return true
+    }
+    return false
 }
