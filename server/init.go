57b3a0d3c8680ac97993427e96254ef285a761bb
Mohamed LN-WSL <114567817+MDDiallo9@users.noreply.github.com>
2025-08-25 15:27:58 +0200

    Changement style et init

--- server/init.go
+++ server/init.go
@@ -10,11 +10,10 @@ var artists []api.Artist
 func InitArtists() []api.Artist {
     artists = api.GetArtists()
     for i := range artists {
-        artists[i].Locations = api.GetLocations(artists[i])
-        artists[i].ConcertDates = api.GetConcertDates(artists[i])
+        /* artists[i].Locations = api.GetLocations(artists[i])
+        artists[i].ConcertDates = api.GetConcertDates(artists[i]) */
         artists[i].Relations = api.GetRelations(artists[i])
     }
-    fmt.Print(artists[2].Locations)
     return artists
 }
 
